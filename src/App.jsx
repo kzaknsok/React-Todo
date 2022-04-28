1664793bf502310dd52c9e2b26e78f7abb1561a9
kzaknsok <89708310+kzaknsok@users.noreply.github.com>
2022-04-29 06:10:50 +0900

    Update App.jsx
    追加機能の実装まで

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,85 @@ import "./styles.css";
  * mapでループ処理した時に見つかった配列をその数だけ出力する。
  * mapは配列の中身を参照、関数と併用してforEachの様に機能できる。
  *
+ * 追加機能実装
+ * inputテキストをincomplete-areaに表示させる
+ * input情報のステート化
+ * useState
+ * 初期値は[""]空文字
+ * 第一引数の変数名todoText,
+ * 更新用の第二引数の変数名setTodoText,
+ * todoTextはinputタグのvalueにあたる
+ * value={todoText}とする
+ * この時点では初期値の空文字が設定されており入力機能が効かない
+ *
+ * inputタグにonChange="{関数名}"を追記して、
+ * stateの更新機能を実装し入力機能を実現させる。
+ * 更新用の変巣を定義、アロー関数で実装、
+ * (e)はeventの略、この部分は自由に命名。
+ * eをターゲットにした処理を書く。
+ * e.target.valueの部分は形式的に覚えてしまう。
+ * それを更新用である変数setTodoTextに渡す。
+ *
+ * inputにonChange={onChangeTodoText}追記
+ * inputを更新するonChangeTodoTextを渡すことで入力機能実装。
+ *
+ * 追加ボタン機能
+ * 追加ボタンのbuttonにonClick={}を追記。
+ * クリックイベントに連動させる変数onClickAddを定義、
+ * alert()で動作確認、alertにはtodoTextを渡す。
+ * その状態でonClickAddをbuttonのonClickに渡す。
+ *
+ * 未完了エリアへの表示
+ * todoTextをincomplete-areaに表示させる
+ * onClickAddに連動させるため、関数内の処理として、
+ * 変数newTodosを実装。
+ * [...incompleteTodos, todoText]
+ * ...３つで配列を取得。
+ * 第二引数にtodoTextを渡す事で配列の更新をする。
+ *
+ * setIncompleteTodos(newTodos)
+ * 更新用ステートincompleteTodosに、
+ * 更新情報のnewTodosを渡して未完了エリアに、
+ * 新しい配列を生成する。
+ *
+ * 現時点での課題...
+ * 「入力欄にテキストが残る」、「空文字のまま追加できる」
+ * クリックと同時に入力欄のステート、setTodoText("")に、
+ * 空文字を設定し直す事で入力欄のリセットを実装。
+ * if (todoText === "") return;をonClickAddの最初に
+ * 組み込むことで空文字の場合は処理を最初に戻すよう設定。
+ * 追加が出来なくなる。
+ * (一回は追加出来てしまう問題。。。未解決)
+ *
+ *
  */
 
 export const App = () => {
+  const [todoText, setTodoText] = useState([""]);
   const [incompleteTodos, setIncompleteTodos] = useState([
     "おおおお",
     "ええええ"
   ]);
   const [completeTodos, setCompleteTodos] = useState(["ああああ"]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="ToDoを入力" />
-        <button>追加</button>
+        <input
+          placeholder="ToDoを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のToDo</p>
